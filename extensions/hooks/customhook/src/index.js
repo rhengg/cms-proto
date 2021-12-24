639ba1197fb07cf8ccdbe4149795912851389f4c
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-12-24 14:09:47 +0530

    add delivery url generator filter before create

--- extensions/hooks/customhook/src/index.js
+++ extensions/hooks/customhook/src/index.js
@@ -1,23 +1,62 @@
-export default ({ filter, action, init }) => {
-	// filter("items.create", (input, { collection }, { schema }) => {
-	// 	console.log("Item being created: ", input);
-	// 	console.log("Item being created collection: ", collection);
-	// 	console.log("Item being created schema: ", schema);
+const resolutions = process.env["AWS_RESOLUTIONS"].split(",");
+const resolutionSuffix = process.env["AWS_OUTPUT_RESOLUTIONS_SUFFIX"].split(
+	","
+);
+const baseURL = process.env["AWS_OUTPUT_BASE_URL"];
 
-	// 	console.log("Creating Item!");
-	// });
-
-	// action("items.create", () => {
-	// 	console.log("Item created!");
-	// });
-
-	// init("app.before", app => {
-	// 	console.log("app before:", app);
-	// });
-
-	filter("roles.delete", (input, meta, context) => {
-		console.log("Role delete check", input);
-		console.log("Role del check meta obj", meta);
-		console.log("Role del check", context);
+const createVideoDeliveryUrls = videoId => {
+	let videoDeliveryUrls = [];
+	resolutions.map((res, index) => {
+		videoDeliveryUrls.push({
+			resolution: res,
+			url: `${baseURL}/${videoId}/${videoId}_${resolutionSuffix[index]}.m3u8`
+		});
 	});
+
+	return videoDeliveryUrls;
 };
+
+export default (
+	{ filter },
+	{ services, exceptions, database, getSchema, env, logger, emitter }
+) => {
+	filter(
+		"movies.items.create",
+		async (input, { collection }, { schema, database }) => {
+			const deliveryUrls = createVideoDeliveryUrls(input.movie_raw);
+
+			return { ...input, movie_delivery_url: deliveryUrls };
+		}
+	);
+
+	// filter("movies.items.update", input => {
+	// 	const suffix = process.env["AWS_OUTPUT_RESOLUTIONS_SUFFIX"];
+	// 	const suffixArray = suffix.split(",");
+	// 	console.log("check console for env vars", suffixArray);
+	// });
+};
+
+// field: movie_delivery_url
+/* 
+  Before create:
+  1. get s3 uuid
+  2. create destination_urls object from env vars
+  3. modify input
+  4. return modified input for next processing
+
+  Before update:
+  1. Check if upload uuid has changed
+  2. If changed, alter destination urls
+  3. Invoke lambda to delete previous video files(raw, input and delivery)
+  3. If unchanged, return input as is.
+*/
+
+// base_url/<uuid>/<uuid>_resolution.m3u8
+
+// 1080: https://myvodservice-dev-output-tmoou92z.s3.ap-south-1.amazonaws.com/7f8ec263-7ea3-4639-af95-3876288fb171/7f8ec263-7ea3-4639-af95-3876288fb171_3000.m3u8
+
+// 540: https://myvodservice-dev-output-tmoou92z.s3.ap-south-1.amazonaws.com/7f8ec263-7ea3-4639-af95-3876288fb171/7f8ec263-7ea3-4639-af95-3876288fb171_1500.m3u8
+
+// 480: https://myvodservice-dev-output-tmoou92z.s3.ap-south-1.amazonaws.com/7f8ec263-7ea3-4639-af95-3876288fb171/7f8ec263-7ea3-4639-af95-3876288fb171_750.m3u8
+
+// 360: https://myvodservice-dev-output-tmoou92z.s3.ap-south-1.amazonaws.com/7f8ec263-7ea3-4639-af95-3876288fb171/7f8ec263-7ea3-4639-af95-3876288fb171_325.m3u8
